530a07ce8c6c8836535170f6cf8257b70c26c643
allyus <allyus@ubcare.co.kr>
2018-03-09 18:22:42 +0900

    수정

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
     render(){
         return (
-                <h1>Hellasdddasdfsadfdfd dd  sss dfasddddd  dsffsdff </h1>
+                <h1>dsffsdff </h1>
         );
     }
 }
